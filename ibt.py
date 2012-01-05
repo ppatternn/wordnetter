1f7508d4ba265d6b5b5a3d33311ed0481b8c6309
Robert Florance <reflorance@robertflorance.com>
2012-01-05 14:59:55 -0700

    v2 update

--- ibt.py
+++ ibt.py
@@ -1,75 +1,102 @@
 """
-The In-Between Thesaurus
-
-Robert Florance 3.21.2011
+In-Between Thesaurus
+Robert Florance 1.4.2012
 """
 
-import urllib2, operator, os, sys
+import requests, operator, sys, simplejson
+class IBT:
 
+    def __init__(self, api_ver, api_key, api_frmt, query1, query2):
+        
+        self.api_ver = api_ver
+        self.api_key = api_key
+        self.api_frmt = api_frmt
+        self.query1 = query1
+        self.query2 = query2
 
-#Basic search function gets synonyms for a single word
-#Currently not using the optional 'format' argument, because it defaults to text
-def query_bighugethesaurus(version, api_key, word, result_format):
+    def query_bht(self, word):
+        
+        url = "http://words.bighugelabs.com/api/" + self.api_ver + "/" + self.api_key +"/" + word + "/" + self.api_frmt
+        r = requests.get(url)
+        c = r.content
+        data = simplejson.loads(c)
+        print "\n ---------------Begin Raw JSON-----------------"
+        print simplejson.dumps((data), indent=2)
+        print "\n ---------------End Raw JSON-------------------"
+        return data
 
-    #Generates a url based on arguments
-    url = "http://words.bighugelabs.com/api/" + version + "/" + api_key +"/" + word + "/"
-    request = urllib2.Request(url)
-    try:
-        response = urllib2.urlopen(request)
-        data = response.read()
-    except urllib2.HTTPError, e:
-        data = e.read()
-
-    return data
-
-
-#This method takes a string delimited by newlines and pipes, and turns them into a list
-def parse_txt_results(data):
-    synonyms = []
-    
-    entries = data.split("\n")
-    entries.remove("")          #need to remove the last empty entry
-    
-    for entry in entries:
-        elements = entry.split("|")
-        if elements[1] == "syn":            #ignore the antonyms
-            word = elements[2]
-            synonyms.append(word)          #add the word to the list of synonyms
+    def parse_adj_synonyms(self, data):
+            syn = []
+            sim = []
+            adj = []
+            if "adjective" in data: adj = data["adjective"]
+            if "syn" in adj: syn = adj["syn"]
+            if "sim" in adj: sim = adj["sim"]
+            e = syn + sim #Return a combined list of similars + synonyms
+            return e
             
-    return synonyms
+    def find_matches(self, set1, set2):
+        matches = []
+        for item in set1:
+            if item in set2:
+                if item not in matches:
+                    matches.append(item)
+        return matches
 
-#This method takes two lists and looks for common entries in each one
-def compare_results(list1, list2):
-    common_results = []
+    def search_deeper(self, set1, set2):
+        bigger_result_set1 = []
+        bigger_result_set2 = []
+        
+        #Find all syn/sims for every result in first search's set
+        for item in set1:
+            r = self.query_bht(item)
+            s = self.parse_adj_synonyms(r)
+            bigger_result_set1 += s
 
-    for entry in list1:
-        if entry in list2:
-            print "Found a common entry!"
-            common_results.append(entry)
+        for item in set2:
+            r = self.query_bht(item)
+            s = self.parse_adj_synonyms(r)
+            bigger_result_set2 += s
+        
+        #look for matches again within this larger set
+        matches = self.find_matches(bigger_result_set1, bigger_result_set2)
+        return matches
 
-    return common_results
+if __name__ == "__main__":
+    version = "2"
+    api_key = "507ab364c2e364a21c82d0ea6118f4c9"
+    result_format = "json" 
+    search1 = sys.argv[1]
+    search2 = sys.argv[2]
+
+    ibt = IBT(version, api_key, result_format, search1, search2)
+
+    #Get full JSON response from big huge thesaurus
+    results1 = ibt.query_bht(search1)
+    results2 = ibt.query_bht(search2)
+    #Filter out only adjective synonyms
+    syns1 = ibt.parse_adj_synonyms(results1)
+    syns2 = ibt.parse_adj_synonyms(results2)
+    #Find matches between the two filtered lists
+    matches = ibt.find_matches(syns1, syns2)
+    #Go deeper
+    further_matches = ibt.search_deeper(syns1, syns2)
+    
+    print matches
+    print further_matches
+    #if len(matches) > 0: print matches, len(matches)
+    #else: print "No relations found."
 
 
-## TESTING ##
-version = "2"
-api_key = "507ab364c2e364a21c82d0ea6118f4c9"
-#word = "fail"
-r_format = None
 
-#Get input from the command line
-word1 = sys.argv[1]
-word2 = sys.argv[2]
 
-print "Searching for words that '" + word1 + "' and '" + word2 + "' have in common..."
 
-results1 = query_bighugethesaurus(version, api_key, word1, r_format)
-results2 = query_bighugethesaurus(version, api_key, word2, r_format)
 
-output1 = parse_txt_results(results1)
-output2 = parse_txt_results(results2)
 
-common_results = compare_results(output1, output2)
 
-print "Results: "
-for result in common_results:
-    print result
+
+
+
+
+
+
