d369612c9de5e0edbc3e6de88a551f6429de9209
reflorance <reflorance@gmail.com>
2014-05-22 04:26:12 -0400

    results can be clicked on to GO DEEPER. kind of just a trap into nothing though right now...

--- static/wordlink.js
+++ static/wordlink.js
@@ -1,15 +1,9 @@
+//TODO: stop writing javascript! i know this is terrible :(
 function on_load() {
     $(function() {
         $("#search").click(
             function(){
-                var base = ""
-                //var base = "http://127.0.0.1:5000"; //TODO: put the wordlink.py host here!
-                $.when(
-                        $.ajax(base + "/word/" + $("#word1").val()),
-                        $.ajax(base + "/word/" + $("#word2").val())
-                    ).done(function(a1, a2){
-                        display_results(a1[0], a2[0]);
-                    });
+                get_data();
             }
         );
         $(".input").keyup(function(event){
@@ -19,24 +13,38 @@ function on_load() {
         });
     });
 }
-
+function get_data(){
+    $.when(
+            $.ajax("/word/" + $("#word1").val()),
+            $.ajax("/word/" + $("#word2").val())
+        ).done(function(a1, a2){
+            display_results(a1[0], a2[0]);
+        });
+}
 //TODO: make this far less dumb!
 function display_results(data1, data2) {
     $(".results").html("");
     $.each(data1.syns, function(k, v){
-        if (is_match(data1.syns[k], data2.syns) == false){
-            $("#result1").append("<div>" + data1.syns[k] + "</div>");
-        } else {
-            $("#result1").append("<div style='color: red'>" + data1.syns[k] + "</div>");
-        }
+        $("#result1").append(display_word(data1.syns[k], is_match(data1.syns[k], data2.syns), "left"));
     });
     $.each(data2.syns, function(k, v){
-        if (is_match(data2.syns[k], data1.syns) == false){
-            $("#result2").append("<div>" + data2.syns[k] + "</div>");
-        } else {
-            $("#result2").append("<div style='color: red'>" + data2.syns[k] + "</div>");
-        }
+        $("#result2").append(display_word(data2.syns[k], is_match(data2.syns[k], data1.syns), "right"));
     });
+    $(".wordlink").click(
+        function(){
+            if (this.classList[1] == ("left"))
+                $("#word1").val(this.innerHTML);
+            else
+                $("#word2").val(this.innerHTML);
+            get_data();
+        }
+    );
+}
+function display_word(word, match, side){
+    if (match == false)
+        return "<div class='wordlink " + side + "'>" + word + "</div>"
+    else
+        return "<div style='color: limegreen' class='wordlink " + side + "'>" + word + "</div>"
 }
 function is_match(item, other_list) {
     if ($.inArray(item, other_list) > -1)
--- wordlink.py
+++ wordlink.py
@@ -19,9 +19,6 @@ def get_synset(term):
     return jsonify(syns)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
-#if __name__ == '__main__':
-#   app.debug = True
-#   app.run()
-
+   app.run(host='0.0.0.0')
+   #app.debug = True
+   #app.run()
