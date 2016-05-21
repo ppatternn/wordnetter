4ff1da71c58d4b31fbb0e7cf54fba96934469a27
reflorance <reflorance@gmail.com>
2016-05-21 06:22:02 -0700

    building back up the synonym matching

--- wordlink.py
+++ wordlink.py
@@ -7,17 +7,28 @@ app = Flask(__name__)
 def home():
     return render_template('wordlink.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/find/')
 def get_wordlinks():
+    term1 = request.args.get('term1')
+    term2 = request.args.get('term2')
+
+    results1 = get_syns_list(term1)
+    results2 = get_syns_list(term2)
+
+    common = list(set(results1) & set(results2))
+
+    return render_template('wordlink.html', results1=results1, results2=results2, common=common)
+
+def get_syns_list(term):
     results = []
-    for ss in Word(request.form['term']).synsets:
+    for ss in Word(term).synsets:
         for ln in ss.lemma_names():
             results.append(ln.encode('ascii', 'replace'))
         for sim in ss.similar_tos():
             for simln in sim.lemma_names():
                 results.append(simln.encode('ascii', 'replace'))
-    syns = {'syns': list(set(results))}
-    return jsonify(syns)
+    return list(set(results))
+
 
 if __name__ == '__main__':
     app.debug = True
